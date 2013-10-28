7ec332550e52dc110ab9897ef7069aaeb481d253
pill <phil.avery@gmail.com>
2013-10-28 16:03:04 -0400

    Update timer_decorator.py

--- decorators/timer_decorator.py
+++ decorators/timer_decorator.py
@@ -1,9 +1,9 @@
 import time
 
 def timer(func):
-    def wrapper(*arg):
+    def wrapper(*arg, **kwargs):
         t1 = time.time()
-        res = func(*arg)
+        res = func(*arg, **kwargs)
         t2 = time.time()
         return res, t2 - t1
     return wrapper
